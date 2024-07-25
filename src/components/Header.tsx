84d5355f4758081fda37b899ef07bb767ffd9e7e
Vinnydaninja <vincenguyen35@gmail.com>
2024-07-25 16:31:00 -0700

    Add layering of profile picture for 3D effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,14 @@ const Header = () => {
     return (
         <header>
             <h1>Hi, I'm Vince Nguyen</h1>
-            <div className="profile-image">
-                <img src="Linkedin_Picture.jpg" alt="picture of me, Vince Nguyen" />
+            <div className="container">
+                <div className="profile-image-crop">
+                    <img src="Linkedin_Picture_Crop.png" alt="picture of me, Vince Nguyen" />
+                </div>
+                <div className="cool-part">I AM GROOOOOT</div>
+                <div className="profile-image">
+                    <img src="Linkedin_Picture.jpg" alt="picture of me, Vince Nguyen" />
+                </div>
             </div>
         </header>
     );
